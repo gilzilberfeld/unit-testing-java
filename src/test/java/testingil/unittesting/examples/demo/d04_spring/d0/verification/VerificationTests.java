cc823ea2f74e62412eaf3b4890fe5482fbeaca7a
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-17 13:39:05 +0300

    added h2 driver

--- src/test/java/testingil/unittesting/examples/demo/d04_spring/d0/verification/VerificationTests.java
+++ src/test/java/testingil/unittesting/examples/demo/d04_spring/d0/verification/VerificationTests.java
@@ -1,10 +1,13 @@
 package testingil.unittesting.examples.demo.d04_spring.d0.verification;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 @SpringBootTest
-class VerificationTests {
+@Ignore
+public class VerificationTests extends AbstractTestNGSpringContextTests {
 
 	@Test
 	void contextLoads() {

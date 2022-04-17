91eae849248e16cf9b7a7117d14c1a5ed8088344
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-17 13:18:44 +0300

    added parameterized test

---
+++ src/test/java/testingil/unittesting/examples/demo/d01/testng/d3_parameterized/FactorialTests.java
@@ -0,0 +1,27 @@
+package testingil.unittesting.examples.demo.d01.testng.d3_parameterized;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import testingil.unittesting.examples.demo.d01.testng.Factorial;
+
+import static org.testng.Assert.assertEquals;
+
+public class FactorialTests {
+
+	@DataProvider(name="factorial_values")
+	public Object[][] factorial_provider(){
+		return new Object[][]{
+				{1, 1},
+				{2, 2},
+				{3, 6},
+				{-3, 0},
+				{10, 3628800}
+		};
+	}
+
+	@Test(enabled = false, dataProvider = "factorial_values")
+	public void factorial_tests(int input, int result) {
+		Factorial factorial = new Factorial();
+		assertEquals(result, factorial.calculate(input));
+	}
+}
